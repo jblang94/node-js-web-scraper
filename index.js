252ccf346cd5a3299051fed336f115fef1ed6885
jblang94 <jbarlang@gmail.com>
2016-08-02 21:13:18 +0000

    finished refactored implementation

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const json2csv = require('json2csv');
 const fs = require('fs');
+
 const url = 'http://substack.net/images/';
 const file = 'images.csv';
-var $ = null;
 
-function createFileInfoObject(tds) {
-  var filePermissions = null;
-  var absoluteUrl = null;
-  var fileType = null;
-  tds.each(function(index, td) {
-    if(index == 0) {
-      filePermissions = td.children[0].children[0].data;
-    }
-    if(index == 2) {
-      absoluteUrl = '/substack.net' + (td.children[0].attribs.href);
-      fileType = absoluteUrl.split('.')[1];
-    }
-  });
-  var fileInfoObject = {"filePermissions": filePermissions, 
-                            "absoluteUrl": absoluteUrl, 
-                            "fileType": fileType};
-  return fileInfoObject;
+function determineFileType(filePermission, url) {
+  if(/d/.test(filePermission)) {
+    return "dir";
+  }
+  var fileExtension = url.split('.').pop();
+  return (fileExtension == url) ? "unknown" : fileExtension;
 }
+
+function createFileInfoJson(tds) {
+  var filePermission = tds.first().text().replace(/[()]/g, '');
+  var url = tds.find('a').attr('href');
+  var fileType = determineFileType(filePermission, url);
+  return { "File Permission": filePermission, 
+              "Absolute Url": '/substack.net' + url, 
+                 "File Type": fileType };
+} 
 
 function streamToCsv(html) {
   var $ = cheerio.load(html);
+  var csvData = [];
   $('tr').each(function(index, tr) {
     var tds = $(tr).children();
-    var fileInfoObject = createFileInfoObject($(tds));
+    var fileInfoJson = createFileInfoJson(tds);
+    csvData.push(fileInfoJson);
+  });
+  var csv = json2csv({ data: csvData, fields: Object.keys(csvData[0])});
+  fs.writeFile(file, csv, function(err) {
+    if(err) throw err;
+    console.log('Saved csv data to ' + file);
   });
 }
 
 request(url, function(error, response, body) {
   if(!error && response.statusCode == 200) {
-    console.log('no error detected');
     streamToCsv(body);
   }
 });
